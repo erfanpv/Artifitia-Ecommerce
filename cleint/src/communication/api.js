06b62cb21893eddce5813ea2f58af57303175357
erfanpv <erfanpv786@gmail.com>
2025-01-28 01:49:43 +0530

    chore(client): update API and Axios interceptor configuration

--- cleint/src/communication/api.js
+++ cleint/src/communication/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const baseUrl = import.meta.env.PORT || "http://localhost:3001/api";
 // console.log(baseUrl);
-const baseUrl = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+const baseUrl = import.meta.env.VITE_API_BASE_URL || "https://artifitia-ecommerce.onrender.com/api";
 
 const API = axios.create({
   baseURL: baseUrl,
--- cleint/src/utils/axiosIntercepter.js
+++ cleint/src/utils/axiosIntercepter.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const baseUrl = import.meta.env.VITE_USER_API;
-console.log(baseUrl);
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: baseUrl,
